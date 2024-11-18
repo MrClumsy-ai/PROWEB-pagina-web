b584a911b5b331dc5bdd438cdbd7716959dc01c9
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2024-11-17 21:46:17 -0600

    modified:   back-end/main.py

--- back-end/main.py
+++ back-end/main.py
@@ -8,15 +8,35 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+class UsuarioBase(BaseModel):
+    IdRol: int
+    Nombre: str
+    Contrasena: str
+    FotoDePerfil: str
+
+class RolBase(BaseModel):
+    Descripcion: str
+
 class PublicacionBase(BaseModel):
     Titulo: str
     IdUsuario: int
     Descripcion: str
 
-class UsuarioBase(BaseModel):
-    IdRol: int
-    Nombre: str
-    Contrasena: str
+class ComentarioBase(BaseModel):
+    IdPublicacion: int
+    Texto: str
+    IdUsuario: int
+
+class HashtagBase(BaseModel):
+    Texto: str
+
+class DetallePublicacionHashtagBase(BaseModel):
+    IdPublicacion: int
+    IdHashtag: int
+
+class LikesBase(BaseModel):
+    IdPublicacion: int
+    IdUsuario: int
 
 def get_db():
     db = SessionLocal()
