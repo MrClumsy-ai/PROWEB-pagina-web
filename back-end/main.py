82b2fee54738594b642483d8b84a257905615749
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2024-11-18 00:06:05 -0600

    modified:   back-end/__pycache__/main.cpython-312.pyc 	modified:   back-end/main.py

--- back-end/main.py
+++ back-end/main.py
@@ -48,21 +48,109 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-# usuarios
-@app.post("/usuarios/", status_code=status.HTTP_201_CREATED)
+# GET
+@app.get("/usuarios/{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_usuario(usuario_id: int, db: db_dependency):
+    db_usuario = db.query(models.Usuario).filter(models.Usuario.Id == usuario_id)
+    if db_usuario is None:
+        raise HTTPException(status_code=404, detail="usuario no encontrado")
+    return db_usuario
+
+@app.get("/roles/{rol_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_rol(rol_id: int, db: db_dependency):
+    db_rol = db.query(models.Rol).filter(models.Rol.Id == rol_id)
+    if db_rol is None:
+        raise HTTPException(status_code=404, detail="rol no encontrado")
+    return db_rol
+
+@app.get("/publicaciones/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_publicacion(publicacion_id: int, db: db_dependency):
+    db_publicacion = db.query(models.Publicacion).filter(models.Publicacion.Id == publicacion_id)
+    if db_publicacion is None:
+        raise HTTPException(status_code=404, detail="publicacion no encontrada")
+    return db_publicacion
+
+@app.get("/comentarios/{comentario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_comentario(comentario_id: int, db: db_dependency):
+    db_comentario = db.query(models.Comentario).filter(models.Comentario.Id == comentario_id)
+    if db_comentario is None:
+        raise HTTPException(status_code=404, detail="comentario no encontrada")
+    return db_comentario
+
+@app.get("/hashtags/{hashtag_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_hashtag(hashtag_id: int, db: db_dependency):
+    db_hashtag = db.query(models.Hashtag).filter(models.Hashtag.Id == hashtag_id)
+    if db_hashtag is None:
+        raise HTTPException(status_code=404, detail="hashtag no encontrada")
+    return db_hashtag
+
+@app.get("/detalle_publicacion_hashtag/{publicacion_id}&{hashtag_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_detalle_publicacion_hashtag(publicacion_id: int, hashtag_id: int, db: db_dependency):
+    db_pubhash = db.query(models.DetallePublicacionHashtag).filter(models.DetallePublicacionHashtag.IdPublicacion == publicacion_id, models.DetallePublicacionHashtag.IdHashtag == hashtag_id)
+    if db_pubhash is None:
+        raise HTTPException(status_code=404, detail="detalle publicacion hashtag no encontrado")
+    return db_pubhash
+
+@app.get("/likes/{publicacion_id}&{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_like(publicacion_id: int, usuario_id: int, db: db_dependency):
+    db_like = db.query(models.Likes).filter(models.Likes.IdPublicacionId == publicacion_id, models.Likes.IdUsuario)
+    if db_like is None:
+        raise HTTPException(status_code=404, detail="like no encontrado")
+    return db_like
+
+@app.get("/likes/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_likes(publicacion_id: int, db: db_dependency):
+    """da la cantidad de likes que tiene una publicacion"""
+    db_likes = db.query(models.Likes).filter(models.Likes.IdPublicacion == publicacion_id).count()
+    if db_likes is None:
+        raise HTTPException(status_code=404, detail="publicacion no encontrada")
+    return db_likes
+
+# POST
+@app.post("/usuarios/", status_code=status.HTTP_201_CREATED, tags=["POST"])
 async def crear_usuario(usuario: UsuarioBase, db: db_dependency):
     db_usuario = models.Usuario(**usuario.dict())
     db.add(db_usuario)
     db.commit()
 
-@app.get("/usuarios/{usuario_id}", status_code=status.HTTP_200_OK)
-async def get_usuario(usuario_id: int, db: db_dependency):
-    usuario = db.query(models.Usuario).filter(models.Usuario.Id == usuario_id)
-    if usuario is None:
-        raise HTTPException(status_code=404, detail="usuario no encontrado")
-    return usuario
+@app.post("/roles/", status_code=status.HTTP_201_CREATED, tags=["POST"])
+async def crear_rol(rol: RolBase, db: db_dependency):
+    db_rol = models.Rol(**rol.dict())
+    db.add(db_rol)
+    db.commit()
 
-@app.delete("/usuarios/{usuario}")
+@app.post("/publicaciones/", status_code=status.HTTP_201_CREATED, tags=["POST"])
+async def crear_publicacion(publicacion: PublicacionBase, db: db_dependency):
+    db_publicacion = models.Publicacion(**publicacion.dict())
+    db.add(db_publicacion)
+    db.commit()
+
+@app.post("/comentarios/", status_code=status.HTTP_201_CREATED, tags=["POST"])
+async def crear_comentario(comentario: ComentarioBase, db: db_dependency):
+    db_comentario = models.Comentario(**comentario.dict())
+    db.add(db_comentario)
+    db.commit()
+
+@app.post("/hashtags/", status_code=status.HTTP_201_CREATED, tags=["POST"])
+async def crear_hashtag(hashtag: HashtagBase, db: db_dependency):
+    db_hashtag = models.Comentario(**hashtag.dict())
+    db.add(db_hashtag)
+    db.commit()
+
+@app.post("/detalle_publicacion_hashtag/", status_code=status.HTTP_201_CREATED, tags=["POST"])
+async def crear_detalle_publicacion_hashtag(pubhash: DetallePublicacionHashtagBase, db: db_dependency):
+    db_pubhash = models.DetallePublicacionHashtag(**pubhash.dict())
+    db.add(db_pubhash)
+    db.commit()
+
+@app.post("/likes/", status_code=status.HTTP_201_CREATED, tags=["POST"])
+async def crear_like(like: LikesBase, db: db_dependency):
+    db_like = models.Likes(**like.dict())
+    db.add(db_like)
+    db.commit()
+
+# DELETE
+@app.delete("/usuarios/{usuario}", status_code=status.HTTP_200_OK, tags=["DELETE"])
 async def borrar_usuario(usuario_id: int, db: db_dependency):
     db_usuario = db.query(models.Usuario).filter(models.Usuario.Id == usuario_id)
     if db_usuario is None:
@@ -70,21 +158,7 @@ async def borrar_usuario(usuario_id: int, db: db_dependency):
     db.delete(db_usuario)
     db.commit()
 
-# roles
-@app.post("/roles/", status_code=status.HTTP_201_CREATED)
-async def crear_rol(rol: RolBase, db: db_dependency):
-    db_rol = models.Rol(**rol.dict())
-    db.add(db_rol)
-    db.commit()
-
-@app.get("/roles/{rol_id}")
-async def get_rol(rol_id: int, db: db_dependency):
-    rol = db.query(models.Rol).filter(models.Rol.Id == rol_id)
-    if rol is None:
-        raise HTTPException(status_code=404, detail="rol no encontrado")
-    return rol
-
-@app.delete("/roles/{rol_id}")
+@app.delete("/roles/{rol_id}", status_code=status.HTTP_200_OK, tags=["DELETE"])
 async def borrar_rol(rol_id: int, db: db_dependency):
     db_rol = db.query(models.Rol).filter(models.Rol.Id == rol_id)
     if db_rol is None:
@@ -92,21 +166,7 @@ async def borrar_rol(rol_id: int, db: db_dependency):
     db.delete(db_rol)
     db.commit()
 
-# publicaciones
-@app.post("/publicaciones/", status_code=status.HTTP_201_CREATED)
-async def crear_publicacion(publicacion: PublicacionBase, db: db_dependency):
-    db_publicacion = models.Publicacion(**publicacion.dict())
-    db.add(db_publicacion)
-    db.commit()
-
-@app.get("/publicaciones/{publicacion_id}", status_code=status.HTTP_200_OK)
-async def get_publicacion(publicacion_id: int, db: db_dependency):
-    publicacion = db.query(models.Publicacion).filter(models.Publicacion.Id == publicacion_id)
-    if publicacion is None:
-        raise HTTPException(status_code=404, detail="publicacion no encontrada")
-    return publicacion
-
-@app.delete("/publicaciones/{publicacion_id}", status_code=status.HTTP_200_OK)
+@app.delete("/publicaciones/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["DELETE"])
 async def borrar_publicacion(publicacion_id: int, db: db_dependency):
     db_publicacion = db.query(models.Publicacion).filter(models.Publicacion.Id == publicacion_id)
     if db_publicacion is None:
@@ -114,22 +174,7 @@ async def borrar_publicacion(publicacion_id: int, db: db_dependency):
     db.delete(db_publicacion)
     db.commit()
 
-
-# comentarios
-@app.post("/comentarios/", status_code=status.HTTP_201_CREATED)
-async def crear_comentario(comentario: ComentarioBase, db: db_dependency):
-    db_comentario = models.Comentario(**comentario.dict())
-    db.add(db_comentario)
-    db.commit()
-
-@app.get("/comentarios/{comentario_id}", status_code=status.HTTP_200_OK)
-async def get_comentario(comentario_id: int, db: db_dependency):
-    comentario = db.query(models.Comentario).filter(models.Comentario.Id == comentario_id)
-    if comentario is None:
-        raise HTTPException(status_code=404, detail="comentario no encontrada")
-    return comentario
-
-@app.delete("/comentarios/{comentario_id}", status_code=status.HTTP_200_OK)
+@app.delete("/comentarios/{comentario_id}", status_code=status.HTTP_200_OK, tags=["DELETE"])
 async def borrar_comentario(comentario_id: int, db: db_dependency):
     db_comentario = db.query(models.Comentario).filter(models.Comentario.Id == comentario_id)
     if db_comentario is None:
@@ -137,24 +182,26 @@ async def borrar_comentario(comentario_id: int, db: db_dependency):
     db.delete(db_comentario)
     db.commit()
 
-# hashtags
-@app.post("/hashtags/", status_code=status.HTTP_201_CREATED)
-async def crear_hashtag(hashtag: HashtagBase, db: db_dependency):
-    db_hashtag = models.Comentario(**hashtag.dict())
-    db.add(db_hashtag)
-    db.commit()
-
-@app.get("/hashtags/{hashtag_id}", status_code=status.HTTP_200_OK)
-async def get_hashtag(hashtag_id: int, db: db_dependency):
-    hashtag = db.query(models.Hashtag).filter(models.Hashtag.Id == hashtag_id)
-    if hashtag is None:
-        raise HTTPException(status_code=404, detail="hashtag no encontrada")
-    return hashtag
-
-@app.delete("/hashtags/{hashtag_id}", status_code=status.HTTP_200_OK)
+@app.delete("/hashtags/{hashtag_id}", status_code=status.HTTP_200_OK, tags=["DELETE"])
 async def borrar_hashtag(hashtag_id: int, db: db_dependency):
     db_hashtag = db.query(models.Hashtag).filter(models.Hashtag.Id == hashtag_id)
     if db_hashtag is None:
         raise HTTPException(status_code=404, detail="hashtag no encontrada")
     db.delete(db_hashtag)
+    db.commit()
+
+@app.delete("/detalle_publicacion_hashtag/{publicacion_id}&{hashtag_id}", status_code=status.HTTP_200_OK, tags=["DELETE"])
+async def borrar_detalle_publicacion_hashtag(publicacion_id: int, hashtag_id: int, db: db_dependency):
+    db_pubhash = db.query(models.DetallePublicacionHashtag).filter(models.DetallePublicacionHashtag.IdPublicacion == publicacion_id, models.DetallePublicacionHashtag.IdHashtag == hashtag_id)
+    if db_pubhash is None:
+        raise HTTPException(status_code=404, detail="detalle publicacion hashtag no encontrada")
+    db.delete(db_pubhash)
+    db.commit()
+
+@app.delete("/likes/{publicacion_id}&{usuario_id}", status_code=status.HTTP_200_OK, tags=["DELETE"])
+async def borrar_like(publicacion_id: int, usuario_id: int, db: db_dependency):
+    db_like = db.query(models.Likes).filter(models.Likes.IdPublicacion == publicacion_id, models.Likes.IdUsuario == usuario_id)
+    if db_like is None:
+        raise HTTPException(status_code=404, detail="hashtag no encontrada")
+    db.delete(db_like)
     db.commit()
