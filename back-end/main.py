06091c27eabd4044b1e038f247cf788708acb23f
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2024-11-18 01:11:59 -0600

    modified:   back-end/__pycache__/main.cpython-312.pyc 	modified:   back-end/main.py

--- back-end/main.py
+++ back-end/main.py
@@ -49,48 +49,78 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 # GET
-@app.get("/usuarios/{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+
+@app.get("/usuario/{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
 async def get_usuario(usuario_id: int, db: db_dependency):
+    """da los detalles de un usuario con su id"""
     db_usuario = db.query(models.Usuario).filter(models.Usuario.Id == usuario_id)
     if db_usuario is None:
         raise HTTPException(status_code=404, detail="usuario no encontrado")
     return db_usuario
-@app.get("/roles/{rol_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+
+@app.get("/rol/{rol_id}", status_code=status.HTTP_200_OK, tags=["GET"])
 async def get_rol(rol_id: int, db: db_dependency):
+    """da los detalles del rol con su id"""
     db_rol = db.query(models.Rol).filter(models.Rol.Id == rol_id)
     if db_rol is None:
         raise HTTPException(status_code=404, detail="rol no encontrado")
     return db_rol
-@app.get("/publicaciones/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+
+@app.get("/publicacion/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["GET"])
 async def get_publicacion(publicacion_id: int, db: db_dependency):
+    """da los detalles de una publicacion con su id"""
     db_publicacion = db.query(models.Publicacion).filter(models.Publicacion.Id == publicacion_id)
     if db_publicacion is None:
         raise HTTPException(status_code=404, detail="publicacion no encontrada")
     return db_publicacion
-@app.get("/comentarios/{comentario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
-async def get_comentario(comentario_id: int, db: db_dependency):
-    db_comentario = db.query(models.Comentario).filter(models.Comentario.Id == comentario_id)
-    if db_comentario is None:
-        raise HTTPException(status_code=404, detail="comentario no encontrada")
-    return db_comentario
-@app.get("/hashtags/{hashtag_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+
+@app.get("/comentarios/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_comentarios_de_publicacion(publicacion_id:int, db: db_dependency):
+    """da un arreglo de comentarios con el id de una publicacion"""
+    db_comentarios = db.query(models.Comentario).filter(models.Comentario.IdPublicacion == publicacion_id)
+    if db_comentarios is None:
+        raise HTTPException(status_code=404, detail="publicacion no encontrada")
+    return db_comentarios
+
+@app.get("/hashtag/{hashtag_id}", status_code=status.HTTP_200_OK, tags=["GET"])
 async def get_hashtag(hashtag_id: int, db: db_dependency):
+    """da un hashtag con su id"""
     db_hashtag = db.query(models.Hashtag).filter(models.Hashtag.Id == hashtag_id)
     if db_hashtag is None:
         raise HTTPException(status_code=404, detail="hashtag no encontrada")
     return db_hashtag
-@app.get("/detalle_publicacion_hashtag/{publicacion_id}&{hashtag_id}", status_code=status.HTTP_200_OK, tags=["GET"])
-async def get_detalle_publicacion_hashtag(publicacion_id: int, hashtag_id: int, db: db_dependency):
-    db_pubhash = db.query(models.DetallePublicacionHashtag).filter(models.DetallePublicacionHashtag.IdPublicacion == publicacion_id, models.DetallePublicacionHashtag.IdHashtag == hashtag_id)
-    if db_pubhash is None:
-        raise HTTPException(status_code=404, detail="detalle publicacion hashtag no encontrado")
-    return db_pubhash
-@app.get("/likes/{publicacion_id}&{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
-async def get_like(publicacion_id: int, usuario_id: int, db: db_dependency):
-    db_like = db.query(models.Likes).filter(models.Likes.IdPublicacionId == publicacion_id, models.Likes.IdUsuario)
-    if db_like is None:
-        raise HTTPException(status_code=404, detail="like no encontrado")
-    return db_like
+
+@app.get("/hashtags/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_hashtags_de_publicacion(publicacion_id: int, db: db_dependency):
+    """da un arreglo de ids de hashtag con el id de la publicacion"""
+    db_hashtags = db.query(models.DetallePublicacionHashtag.IdHashtag).filter(models.DetallePublicacionHashtag.IdPublicacion == publicacion_id)
+    if db_hashtags is None:
+        raise HTTPException(status_code=404, detail="publicacion no encontrada")
+    return db_hashtags
+
+# @app.get("/detalle_publicacion_hashtag/{publicacion_id}&{hashtag_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+# async def get_detalle_publicacion_hashtag(publicacion_id: int, hashtag_id: int, db: db_dependency):
+#     """da los detalles con los id de los 2"""
+#     db_pubhash = db.query(models.DetallePublicacionHashtag).filter(models.DetallePublicacionHashtag.IdPublicacion == publicacion_id, models.DetallePublicacionHashtag.IdHashtag == hashtag_id)
+#     if db_pubhash is None:
+#         raise HTTPException(status_code=404, detail="detalle publicacion hashtag no encontrado")
+#     return db_pubhash
+
+# @app.get("/like/{publicacion_id}&{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+# async def get_like(publicacion_id: int, usuario_id: int, db: db_dependency):
+#     """da un like con el id de publicacion y del usuario"""
+#     db_like = db.query(models.Likes).filter(models.Likes.IdPublicacionId == publicacion_id, models.Likes.IdUsuario)
+#     if db_like is None:
+#         raise HTTPException(status_code=404, detail="like no encontrado")
+#     return db_like
+
+@app.get("/publicaciones/{usuario_id}", status_code=status.HTTP_200_OK, tags=["GET"])
+async def get_publicaciones_de_usuario(usuario_id: int, db: db_dependency):
+    """da las publicaciones de un usuario"""
+    db_publicaciones = db.query(models.Publicacion).filter(models.Publicacion.IdUsuario == usuario_id)
+    if db_publicaciones is None:
+        raise HTTPException(status_code=404, detail="usuario no encontrado")
+    return db_publicaciones
 
 # contadores
 @app.get("/contadores/likes/{publicacion_id}", status_code=status.HTTP_200_OK, tags=["CONTADORES"])
